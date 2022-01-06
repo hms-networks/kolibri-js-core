ffd0c13bf3fcc32bf1e72414ac2e015dc1824c79
Artjom Büchert <artjom.buechert@hms-networks.com>
2022-01-06 09:02:46 +0100

    chore: update npm dependencies

--- jest.config.js
+++ jest.config.js
@@ -136,7 +136,7 @@ module.exports = {
     setupFiles: ['./test/jest-setup.ts'],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
-    setupFilesAfterEnv: ['jest-extended'],
+    setupFilesAfterEnv: ['jest-extended/all'],
 
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
     // snapshotSerializers: [],
